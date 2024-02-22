7c077ae5ced69a789c7a637dd8df9449fb3529e9
wei <alex20180614@gmail.com>
2024-02-22 14:49:13 -0500

    create a create function

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+// import { data } from 'autoprefixer'
+// import React from 'react'
+import prisma from '../utils/db'
+import { revalidatePath } from 'next/cache'
+
+// The FormData object, which contains the data submitted in the form
+const createTask = async (formData) => {
+  'use server'
+  const content = formData.get('content')
+  // console.log(content)
+  await prisma.task.create({
+    data: {
+      content
+    }
+  })
+  revalidatePath('/tasks')
+
+}
 
 const TaskForm = () => {
   return (
-    <div>taskForm</div>
+    // action
+    <form action={createTask}>
+      <div className="join w-full">
+        <input type="text"
+          className='input input-bordered join-item w-full'
+          placeholder="type here"
+          name='content'
+          required
+        />
+        <button type='submit' className="btn btn-primary join-item">
+          Create task
+
+        </button>
+      </div>
+    </form>
   )
 }
 
